d1fc08635f8f63f05268d3c4ae7f270422253780
Paul Diaz <paulcardozo_01@outlook.com>
2021-07-19 14:54:11 -0500

    Se agregan variables en vuex
    -Se adiciona variables en el state
    
    Aún no se encuentran en uso

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    apiKey: "eri0oGt9xiX5y78RKPhvoWfCQmyGQDUG",
+    URL: "https://api.giphy.com/v1/gifs/trending"
   },
   mutations: {
   },
