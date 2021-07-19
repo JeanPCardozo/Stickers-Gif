d752d7c2224e02494f2c2fc2da6c9680c900a79d
Paul Diaz <paulcardozo_01@outlook.com>
2021-07-19 14:51:50 -0500

    Se agregan vistas
    - Se elimina la vista de about
    - se agrega la vista donde se verán todos los stickers
    - se agrega la vista donde se averán todos los gifs y su lógica
      utilizando axios para peticiones con la api GIPHY

---
+++ src/views/AllGifs.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="allGifs">
+    <p class="is-size-2 ">Gifs</p>
+    <hr />
+    <div class="control columns is-mobile is-multiline is-centered">
+      <div class="column is-9-desktop is-6-tablet is-8-mobile">
+        <input
+          class="input"
+          type="text"
+          v-model="search"
+          placeholder="Buscar Gif...."
+        />
+      </div>
+      <div class="column is-2-desktop is-2-tablet is-2-mobile">
+        <button
+          class="button is-success"
+          :class="load ? 'is-loading' : ''"
+          @click="gifFiltered"
+        >
+          Buscar
+        </button>
+      </div>
+    </div>
+
+    <loading v-if="pageLoad" />
+
+    <div v-else class="columns is-mobile is-multiline is-centered">
+      <Gifs
+        v-for="gif in gifs"
+        :key="gif.id"
+        :src="gif.images.downsized.url"
+        :title="gif.title"
+        :user="gif.user"
+        class="column is-6-mobile is-3-tablet is-2-desktop"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Gifs from "../components/Gifs.vue";
+import loading from "../components/loading.vue";
+export default {
+  data: () => ({
+    gif: {},
+    filterGif: {},
+    search: "",
+    load: false,
+    pageLoad: false,
+    apiKey: "eri0oGt9xiX5y78RKPhvoWfCQmyGQDUG",
+  }),
+  components: {
+    Gifs,
+    loading,
+  },
+  computed: {
+    gifs() {
+      return this.search != "" ? this.filterGif : this.gif;
+    },
+  },
+  methods: {
+    async getTrendingGifs() {
+      this.pageLoad = true;
+      const URL = "https://api.giphy.com/v1/gifs/trending";
+      const { data } = await this.axios.get(`${URL}?api_key=${this.apiKey}`);
+      this.gif = data.data;
+      this.pageLoad = false;
+    },
+    async gifFiltered() {
+      this.load = true;
+      this.pageLoad = true;
+      const URL = "https://api.giphy.com/v1/gifs/search";
+      const { data } = await this.axios.get(
+        `${URL}?api_key=${this.apiKey}&q=${this.search}`
+      );
+      let filtered = data.data;
+      this.filterGif = filtered;
+      this.load = false;
+      this.pageLoad = false;
+    },
+  },
+  mounted() {
+    this.getTrendingGifs();
+  },
+};
+</script>
---
+++ src/views/AllStickers.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="allGifs">
+    <p class="is-size-2 ">Stickers</p>
+    <hr />
+    <div class="field">
+      <div class="control columns is-mobile is-multiline is-centered">
+        <div class="column is-9-desktop is-6-tablet is-8-mobile">
+          <input class="input" type="text" placeholder="Buscar Gif...." />
+        </div>
+        <div class="column is-2-desktop is-2-tablet is-2-mobile">
+          <button class="button is-success">Buscar</button>
+        </div>
+      </div>
+    </div>
+    <div class="columns is-mobile is-multiline is-centered">
+      <Stickers class="column is-6-mobile is-3-tablet is-2-desktop" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Stickers from "../components/Stickers.vue";
+export default {
+  data: () => ({}),
+  components: {
+    Stickers,
+  },
+};
+</script>
