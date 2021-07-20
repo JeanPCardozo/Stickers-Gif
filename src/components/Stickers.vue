2525615b398b0090566d7322c9f8288c8cb23353
Paul Diaz <paulcardozo_01@outlook.com>
2021-07-19 19:33:15 -0500

    Se agrega toda la lógica de programación para obtener los sticker

--- src/components/Stickers.vue
+++ src/components/Stickers.vue
@@ -3,21 +3,29 @@
     <div class="card">
       <div class="card-image">
         <figure class="image is-4by3">
-          <img
-            src="https://bulma.io/images/placeholders/1280x960.png"
-            alt="Placeholder image"
-          />
+          <img :src="src" alt="Placeholder image" />
         </figure>
       </div>
       <div class="card-content">
-        <div class="media">
+        <div v-if="user != {}" class="media">
+          <div class="media-left">
+            <figure class="image is-48x48">
+              <img :src="user.avatar_url" alt="avatar user" />
+            </figure>
+          </div>
           <div class="media-content">
-            <p class="title is-4">Name</p>
+            <p class="title is-4">{{ user.username }}</p>
+            <p class="subtitle is-6">
+              <a :href="user.website_url" target="_blank">{{
+                user.website_url
+              }}</a>
+            </p>
           </div>
         </div>
 
         <div class="content">
-          Descripción
+          <p class="is-size-4">{{ title }}</p>
+          <p v-if="user != {}">{{ user.description }}</p>
         </div>
       </div>
       <footer class="card-footer">
@@ -30,7 +38,16 @@
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    src: String,
+    title: String,
+    user: {
+      type: Object,
+      default: {},
+    },
+  },
+};
 </script>
 
 <style></style>
--- src/views/AllStickers.vue
+++ src/views/AllStickers.vue
@@ -5,25 +5,83 @@
     <div class="field">
       <div class="control columns is-mobile is-multiline is-centered">
         <div class="column is-9-desktop is-6-tablet is-8-mobile">
-          <input class="input" type="text" placeholder="Buscar Gif...." />
+          <input
+            class="input"
+            type="text"
+            v-model="search"
+            placeholder="Buscar Sticker...."
+          />
         </div>
         <div class="column is-2-desktop is-2-tablet is-2-mobile">
-          <button class="button is-success">Buscar</button>
+          <button
+            class="button is-success"
+            :class="load ? 'is-loading' : ''"
+            @click="stickerFiltered"
+          >
+            Buscar
+          </button>
         </div>
       </div>
     </div>
-    <div class="columns is-mobile is-multiline is-centered">
-      <Stickers class="column is-6-mobile is-3-tablet is-2-desktop" />
+    <loading v-if="pageLoad" />
+    <div v-else class="columns is-mobile is-multiline is-centered">
+      <Stickers
+        v-for="sticker in stickers"
+        :key="sticker.id"
+        :src="sticker.images.downsized.url"
+        :title="sticker.title"
+        :user="sticker.user"
+        class="column is-6-mobile is-3-tablet is-2-desktop"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import Stickers from "../components/Stickers.vue";
+import loading from "../components/loading.vue";
 export default {
-  data: () => ({}),
+  data: () => ({
+    sticker: {},
+    filterSticker: {},
+    search: "",
+    load: false,
+    pageLoad: false,
+    apiKey: "eri0oGt9xiX5y78RKPhvoWfCQmyGQDUG",
+  }),
   components: {
     Stickers,
+    loading,
+  },
+  computed: {
+    stickers() {
+      return this.search != "" ? this.filterSticker : this.sticker;
+    },
+  },
+  methods: {
+    async getTrendingStickers() {
+      this.pageLoad = true;
+      const URL = "https://api.giphy.com/v1/stickers/trending";
+      const { data } = await this.axios.get(`${URL}?api_key=${this.apiKey}`);
+      this.sticker = data.data;
+      this.pageLoad = false;
+    },
+    async stickerFiltered() {
+      this.load = true;
+      this.pageLoad = true;
+      const URL = "https://api.giphy.com/v1/stickers/search";
+      const { data } = await this.axios.get(
+        `${URL}?api_key=${this.apiKey}&q=${this.search}`
+      );
+      let filtered = data.data;
+      this.filterSticker = filtered;
+      console.log(this.filterSticker);
+      this.load = false;
+      this.pageLoad = false;
+    },
+  },
+  mounted() {
+    this.getTrendingStickers();
   },
 };
 </script>
